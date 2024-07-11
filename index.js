cb19e11d85d2555ef5ebc8ef638db3e03b9f100c
Catopish <athila2001@gmail.com>
2024-07-11 18:15:40 +0700

    tes lagi

--- index.js
+++ index.js
@@ -50,45 +50,45 @@ app.get("/r/unixporn", (req, res) => {
   }
 });
 
-// app.get("/r/:subreddit", async (req, res) => {
-//   try {
-//     console.log(`Fetching subreddit: ${req.params.subreddit}`);
-//     const { subreddit } = req.params;
-//     const response = await axios.get(
-//       uDpKkmEd0OYwwR006SPlB6R3efpRHw`https://www.reddit.com/r/${subreddit}/hot.json`,
-//     );
-//     console.log(`Reddit API response status: ${response.status}`);
-//     const redditApi = response.data.data.children;
-//
-//     if (redditApi && redditApi.length > 0) {
-//       console.log(`Rendering subreddit page with ${redditApi.length} posts.`);
-//       res.render("subreddit", { redditApi });
-//     } else {
-//       console.log("No posts found or subreddit does not exist.");
-//       res.send("No posts found or subreddit does not exist.");
-//     }
-//   } catch (error) {
-//     console.error(`Error fetching subreddit data: ${error.message}`);
-//     res.status(500).send("An error occurred while fetching subreddit data.");
-//   }
-// });
 app.get("/r/:subreddit", async (req, res) => {
-  const { subreddit } = req.params;
-  const response = await axios.get(
-    `https://www.reddit.com/r/${subreddit}/hot.json`,
-  );
-  const redditApi = response.data.data.children;
   try {
-    if (redditApi) {
+    console.log(`Fetching subreddit: ${req.params.subreddit}`);
+    const { subreddit } = req.params;
+    const response = await axios.get(
+      uDpKkmEd0OYwwR006SPlB6R3efpRHw`https://www.reddit.com/r/${subreddit}/hot.json`,
+    );
+    console.log(`Reddit API response status: ${response.status}`);
+    const redditApi = response.data.data.children;
+
+    if (redditApi && redditApi.length > 0) {
+      console.log(`Rendering subreddit page with ${redditApi.length} posts.`);
       res.render("subreddit", { redditApi });
     } else {
-      res.send("failed");
+      console.log("No posts found or subreddit does not exist.");
+      res.send("No posts found or subreddit does not exist.");
     }
   } catch (error) {
-    console.error(error);
+    console.error(`Error fetching subreddit data: ${error.message}`);
     res.status(500).send("An error occurred while fetching subreddit data.");
   }
 });
+// app.get("/r/:subreddit", async (req, res) => {
+//   const { subreddit } = req.params;
+//   const response = await axios.get(
+//     `https://www.reddit.com/r/${subreddit}/hot.json`,
+//   );
+//   const redditApi = response.data.data.children;
+//   try {
+//     if (redditApi) {
+//       res.render("subreddit", { redditApi });
+//     } else {
+//       res.send("failed");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("An error occurred while fetching subreddit data.");
+//   }
+// });
 
 app.listen(3000, () => {
   console.log("Port 3000");
